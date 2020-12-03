37a813c58152f9dc9800b2898bfa7e5feb8a92af
lakshay129 <71269556+lakshay129@users.noreply.github.com>
2020-12-03 19:26:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+var dog,happydog,database;
+var happydogi,dogi;
+var foodS,foodStack;
+var addFoods,feedDog;
+var fedTime,lastFed;
+var foodObj;
+
+function preload()
+{
+  dogi=loadImage("Dog.png");
+  happydogi=loadImage("happydog.png");
+}
+
+function setup() {
+  createCanvas(600, 700);
+  
+  dog=createSprite(250,400,4,5);
+  dog.scale=0.5;
+  dog.addImage(dogi)
+
+ foodObj=new Food();
+//referring to firebase database 
+  database=firebase.database();
+
+ //referring foodStack to food node in database
+  foodStack=database.ref('food')
+
+ //On func. is used to read value of foodStack from readData  
+  foodStack.on("value",readData);
+
+  feed =createButton("feed the dog");
+  feed.position(450,55)
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(550,55);
+  addFood.mousePressed(addFoods);
+  
+
+
+}
+
+
+function draw() {  
+  background("blue");
+
+  foodObj.display();
+
+//reading lastFed time from database
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  })
+
+//texted lastFeed time
+  fill(225,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed : "+lastFed%12 + "PM",700,55);
+  }else if(lastFed==0){
+    text("Last Feed :12 AM",300,45);
+  } else {
+    text("Last Feed : "+ lastFed +"AM",700,55)
+  }
+
+  drawSprites();
+  textSize(20)
+  text ("PRESS UP_ARROW to Feed the dog",600,400);
+
+}
+
+//readData is a func. help in catching value from database
+function readData(data){
+  foodS=data.val();
+}
+
+// writeStock is a func. to write value in database
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    food:x
+  })
+}
+
+function addFoods(){
+foodS++;
+database.ref('/').update({
+  Food:foodS
+})
+}
+
+function feedDog(){
+  dog.addImage(happydogi);
+
+  foodObj.updateFoodStack(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStack(),
+    FeedTime:hour()
+  })
+}
+
+console.log(foodObj)
+
